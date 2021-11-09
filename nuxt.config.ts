bf304352f0557b4fc6336f60bd1beefd2171001c
Daisuke Fujimoto <glamseam@gmail.com>
2021-11-09 18:54:34 +0900

    core: add vite settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,29 @@
 import { defineNuxtConfig } from 'nuxt3'
+import path from 'path'
 
 export default defineNuxtConfig({
+    srcDir: 'src/',
+    css: [
+        '@scss/style.scss'
+    ],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@use "@scss/override-variables";'
+                }
+            }
+        },
+        optimizeDeps: {
+            include: [
+                'lottie-web',
+                'prismjs'
+            ]
+        },
+        resolve: {
+            alias: {
+                '@scss': path.join(__dirname, 'src/assets/scss')
+            }
+        }
+    }
 })
